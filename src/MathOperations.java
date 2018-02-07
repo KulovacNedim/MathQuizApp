5b7d6d3985a82499705e534cc58b1158e194c027
Kulovac Nedim <nedim.kulovac@gmail.com>
2018-02-07 23:50:24 +0100

    inition commit

---
+++ src/MathOperations.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class MathOperations {
+
+	public void additionOperation(Scanner sc) {
+		
+		
+	}
+
+	public void substractionOperation(Scanner sc) {
+		
+		
+	}
+
+	public void multiplicationOperation(Scanner sc) {
+		
+		
+	}
+
+	public void divisionOperation(Scanner sc) {
+		
+		
+	}
+
+	public void squaringOperation(Scanner sc) {
+		
+		
+	}
+
+	public void rootingOperation(Scanner sc) {
+		
+		
+	}
+
+}
---
+++ src/MathQuizApp.java
@@ -0,0 +1,65 @@
+
+import java.util.Scanner;
+
+public class MathQuizApp {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		MathOperations mathOp = new MathOperations();
+
+		System.out.println("+----------------------------------------+");
+		System.out.println("+                                        +");
+		System.out.println("+     Welcome to Math Quiz Aplication!   +");
+		System.out.println("+                                        +");
+		System.out.println("+----------------------------------------+");
+
+		System.out.println("\nChoose qiuz you want to try: \n");
+		System.out.println("    1) Addition");
+		System.out.println("    2) Substraction");
+		System.out.println("    3) Multiplication");
+		System.out.println("    4) Division");
+		System.out.println("    5) Square rooting");
+		System.out.println("    6) Squaring");
+
+		System.out.print("\nEnter your choice: ");
+
+		int choice = sc.nextInt();
+
+		switch (choice) {
+		case 1:
+			System.out.println("\nWelcome to Addition Quiz!\n");
+
+			mathOp.additionOperation(sc);
+			break;
+		case 2:
+			System.out.println("\nWelcome to Substraction Quiz!\n");
+
+			mathOp.substractionOperation(sc);
+			break;
+		case 3:
+			System.out.println("\nWelcome to Multiplication Quiz!\n");
+
+			mathOp.multiplicationOperation(sc);
+			break;
+		case 4:
+			System.out.println("\nWelcome to Division Quiz!\n");
+
+			mathOp.divisionOperation(sc);
+			break;
+		case 5:
+			System.out.println("\nWelcome to Square Rooting Quiz!\n");
+
+			mathOp.squaringOperation(sc);
+			break;
+		case 6:
+			System.out.println("\nWelcome to Squaring Quiz!\n");
+
+			mathOp.rootingOperation(sc);
+			break;
+		default:
+		}
+
+	}
+
+}
